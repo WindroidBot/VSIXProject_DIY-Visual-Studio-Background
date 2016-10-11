22e1f693b5def5ba5c8d89bdaa5f549b7f50c221
Windroid Xie <xiexy1995@126.com>
2016-10-12 00:07:38 +0800

    调♀教VS姬

---
+++ VSIXProject/BackgroundPackage.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio;
+//using Company.VSIXProject;////注意改掉此句中的VSPackage ，改成你的项目名称，如若找不到
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Moen.IDEBackground
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    //[Guid(GuidList.guidVSIXProjectPkgString)]//注意改掉此句中的VSPackage ，改成你的项目名称，如若找不到，可在Guids.cs寻找变量名替换,头文件也需改变
+    [ProvideAutoLoad(UIContextGuids.NoSolution)]
+    [ProvideAutoLoad(UIContextGuids.SolutionExists)]
+    public sealed class IDEBackgroundPackage : Package
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            Application.Current.MainWindow.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var rWindow = (Window)sender;
+
+            var rImageSource = BitmapFrame.Create(new Uri(@"C:\Users\xiexy\Pictures\05dd9a1d61941f9e7bf42d0ffcb843d7_r.jpg"/*图片路径*/), BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            rImageSource.Freeze();
+
+            var rImageControl = new Image()
+            {
+                Source = rImageSource,
+                Stretch = Stretch.UniformToFill,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+
+            Grid.SetRowSpan(rImageControl, 4);
+            var rRootGrid = (Grid)rWindow.Template.FindName("RootGrid", rWindow);
+            rRootGrid.Children.Insert(0, rImageControl);
+        }
+    }
+}
